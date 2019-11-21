08af3bfb0065656c5f5239d8cff5a59e86116c75
Jeff Byrnes <Jrbyrnes1989@gmail.com>
2019-11-20 20:05:43 -0800

    Working version of sleep

--- working.c
+++ working.c
@@ -92,23 +92,6 @@ struct TCB
 };
 
 
-/***********************
-function definitions
-
-
-***********************/
-void stateTransition(int);
-struct TCB scheduler(int);
-void dispatch(void);
-void f(void);
-void g(void);
-void h(void);
-int createThread(void (*fPtr)(void));
-int getMyID(void);
-void sleepThread(int);
-void unsleep(int);
-
-
 
 /***********************
 linked-list definition & API
@@ -192,12 +175,7 @@ struct TCB pop(struct linkedList* list)
     */
 
     struct listNode *temp = list->head;
-    if (list->size > 1)
-        list->head = list->head->next;
-    else
-    {
-        list->head = (struct listNode *)malloc(sizeof(struct listNode));
-    }
+    list->head = list->head->next;
     struct TCB y = temp->x;
     //free(temp);
 
@@ -274,11 +252,9 @@ struct TCB curr;
 struct TCB next;
 struct TCB thrArr[MAX_THREADS]; //uncomment for thrArr
 struct linkedList readyQueue;
-struct linkedList sleepQueue;
 enum State{Running = 0, Ready = 1, Asleep = 2, Done = 3};
 struct itimerval timer;
 int schedule;
-int sleeping = 0; //flag to determine whether or not to put thread to sleep
 
 
 
@@ -289,11 +265,13 @@ functions
 ************************/
 
 
+
 void stateTransition(int newState)
 {
     gettimeofday(&curr.stats.timeEnd, NULL);
     double window = (double) (curr.stats.timeEnd.tv_usec - curr.stats.timeStart.tv_usec) / 1000000 + (double)(curr.stats.timeEnd.tv_sec - curr.stats.timeStart.tv_sec);
 
+    //printf("\n\n%f\n", window);
 
     switch(curr.state)
     {
@@ -318,8 +296,6 @@ void stateTransition(int newState)
 
     curr.state = newState;
     gettimeofday(&curr.stats.timeStart, NULL);
-
-
 }
 
 
@@ -337,54 +313,95 @@ struct TCB scheduler(int sched)
         int r = rand() % getSize(&readyQueue);
         int n = getSize(&readyQueue);
         popped = popN(r, &readyQueue);
-        return popped;      
+        return popped;
+        
     }
+
 }
 
 
 void dispatch()
 {
-
     next = scheduler(schedule);
-    
-    if (sleeping == 0)
-        stateTransition(Ready);
 
-    else
-    {
-        stateTransition(Asleep);
-    }
-
+    stateTransition(Ready);
 
     //in the first run of dispatch, curr will be null
     if (curr.sp)
     {
-        if (sleeping == 0)
-        {
-            addToEnd(curr, &readyQueue);
-            thrArr[curr.tid] = curr;
-        }
-
-        else
-        {
-            addToEnd(curr, &sleepQueue);
-            thrArr[curr.tid] = curr;
-            sleeping = 0; //reset the flag
-        }
+        addToEnd(curr, &readyQueue);
+        thrArr[curr.tid] = curr;
     }
 
 
     curr = next;
+
+    
+    /*
+    int ret_val = sigsetjmp(jbuf[x],1);
+    printf("SWITCH: ret_val=%d\n", ret_val); 
+    if (ret_val == 1) {
+        return;
+    }
+    */
+    
+    int x = curr.tid;
     stateTransition(Running);
-    if (curr.tid == 0)
+    if (x == 0)
     {
         printf("\n\nthread 0 runtime: %lf\n", curr.stats.runTime);
         printf("thread 0 waittime: %lf\n", curr.stats.waitTime);
-        printf("thread 0 sleeptime: %lf\n", curr.stats.sleepTime);
     }
-
-    siglongjmp(jbuf[curr.tid],1);
+    siglongjmp(jbuf[x],1);
 }
+
+/*
+void f( void )
+{
+    printf("b\n");
+    int a=1;
+    printf("c\n");
+    int i=0;
+    while(1) {
+        ++i;
+        printf("in f (%d)\n",i);
+        if (i % 3 == 0) {
+            printf("f: switching\n");
+            dispatch();
+        }
+        usleep(SECOND);
+    }
+}
+
+void g( void )
+{
+    int i=0;
+    while(1){
+        ++i;
+        printf("in g (%d)\n",i);
+        if (i % 5 == 0) {
+            printf("g: switching\n");
+            dispatch();
+        }
+        usleep(SECOND);
+    }
+}
+
+void h( void)
+{
+    int i = 0;
+    while (1)
+    {
+        ++i;
+        printf("in h (%d)\n",i);
+        if (i % 8 == 0){
+            printf("h: switching\n");
+            dispatch();
+        }
+        usleep(SECOND);
+    }
+}
+*/
 
 
 void f( void ) {
@@ -396,8 +413,8 @@ void f( void ) {
         ++i;
         if (i % 99999555 == 0) 
         {
-            printf("f: sleeping\n");
-            sleepThread(2);
+            printf("f: switching\n");
+            dispatch();
         }
 
         int j;
@@ -428,7 +445,7 @@ void g( void )
 
 void h( void )
 {
-    //printf("My ID is: %d\n", getMyID()); 
+    //printf("My ID is: %d\n", GetMyID()); 
     int i=0;
 
     while(1)
@@ -445,44 +462,6 @@ void h( void )
     }
 }
 
-
-void sleepThread(int sec)
-{
-    sleeping = 1;
-    alarm(sec);
-    dispatch();
-}
-
-void unsleep(int signum)
-{
-    struct TCB temp = pop(&sleepQueue);
-
-    /*
-    code re-use from stateChange
-    if I want to use the stateChange function then
-    add a second parameter that takes the TCB node
-    return that TCB node from stateChange
-    have the node passed be the value that receives assign
-    (LHS)
-    apply fixes to dispatch
-    this function would look like
-        temp = stateChange(Ready, temp);
-        thrArr[temp.tid] = temp;
-        addToEnd(temp, &readyQueue);
-    */
-    gettimeofday(&temp.stats.timeEnd, NULL);
-    double window = (double) (temp.stats.timeEnd.tv_usec - temp.stats.timeStart.tv_usec) / 1000000 + (double)(temp.stats.timeEnd.tv_sec - temp.stats.timeStart.tv_sec);
-    ++temp.stats.nSleeps;
-    temp.stats.sleepTime += window;
-    temp.stats.avgSleep = curr.stats.sleepTime / curr.stats.nSleeps;
-    temp.state = Ready;
-    gettimeofday(&temp.stats.timeStart, NULL);
-
-    thrArr[temp.tid] = temp;
-    addToEnd(temp, &readyQueue);
-    
-    dispatch();
-}
 
 
 
@@ -520,10 +499,7 @@ int createThread(void (*fPtr)(void))
     return threadNode.tid;
 }
 
-int getMyID()
-{
-    return curr.tid;
-}
+
 
 
 /************************
@@ -539,18 +515,14 @@ int main()
     } while (schedule != 0 && schedule != 1);
     
 
-    //most of this stuff should be in go()
     readyQueue = createList();
-    sleepQueue = createList();
 
-
-    createThread(f);         
+    createThread(f);        
     createThread(g);
     createThread(h);
 
 
-    signal(SIGVTALRM, dispatch);
-    signal(SIGALRM, unsleep);
+    signal(SIGVTALRM,dispatch);
     
     timer.it_value.tv_sec = 2;
     timer.it_value.tv_usec = 0;
@@ -559,10 +531,13 @@ int main()
 
     setitimer(ITIMER_VIRTUAL, &timer, NULL);
 
+    
+
+    
+    
 
     dispatch();
     
     return 0;
 }
-
 
